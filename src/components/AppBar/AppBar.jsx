5ce7f3ad9e26af00e40ca0d842479cc37481b99b
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-08-11 22:17:12 +0300

    isLoggedIn works

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -3,13 +3,15 @@ import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import css from "../AppBar/AppBar.module.css";
+import { useSelector } from "react-redux";
 
 function AppBar() {
+    const isLoggedIn =useSelector(selectIsLoggedIn);
   return (
     <header className={css.header}>
       <nav>
         <Navigation />
-        {selectIsLoggedIn ? <UserMenu /> : <AuthNav />}
+        {isLoggedIn ? <UserMenu />:<AuthNav />}
       </nav>
     </header>
   );
