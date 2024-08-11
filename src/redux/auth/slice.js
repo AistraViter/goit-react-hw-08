6d0a8d9bbec03efbe99f8319f9beb1fd99abc7f2
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-08-12 00:28:53 +0300

    Registration completed

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { register } from "./operations";
 
 const authSlice = createSlice({
   name: "auth",
   initialState: {
-  user: {
-    name: null,
-    email: null,
+    user: {
+      name: null,
+      email: null,
+    },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
   },
-  token: null,
-  isLoggedIn: false,
-  isRefreshing: false,
-}});
+  extraReducers: (builder) =>
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    }),
+});
 
 export default authSlice.reducer;
-
