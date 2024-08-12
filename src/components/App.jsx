42591c0dd45af3bdc557b9912efa5a6b4a98bc4b
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-08-12 08:50:11 +0300

    RestrictedRoute completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,12 @@
 import { lazy, Suspense, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import AppBar from "./AppBar/AppBar";
-import "normalize.css";
 import { useDispatch, useSelector } from "react-redux";
+import "normalize.css";
+import AppBar from "./AppBar/AppBar";
+import  RestrictedRoute  from "./RestrictedRoute/RestrictedRoute";
 import { refreshUser } from "../redux/auth/operations";
-import {  selectIsRefreshing } from "../redux/auth/selectors";
+import { selectIsRefreshing } from "../redux/auth/selectors";
 import css from "./App.module.css";
-
-
 
 const HomePage = lazy(() => import("../pages/HomePage/HomePage"));
 const ContactsPage = lazy(() => import("../pages/ContactsPage/ContactsPage"));
@@ -19,21 +18,39 @@ const NotFoundPage = lazy(() => import("../pages/NotFoundPage/NotFoundPage"));
 
 function App({ errorMessage }) {
   const dispatch = useDispatch();
-  const isRefreshing = useSelector(selectIsRefreshing)
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return  isRefreshing ? (<div className={css.apploading}>Refreshing...</div>) : ( 
+  return isRefreshing ? (
+    <div className={css.apploading}>Refreshing...</div>
+  ) : (
     <div>
       {<AppBar />}
       <Suspense fallback={<div className={css.apploading}>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage errorMessage={errorMessage} />} />
           <Route path="/contacts" element={<ContactsPage />} />
-          <Route path="/register" element={<RegistrationPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                component={<RegistrationPage />}
+                redirectTo="/contacts"
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                component={<LoginPage />}
+                redirectTo="/contacts"
+              />
+            }
+          />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
